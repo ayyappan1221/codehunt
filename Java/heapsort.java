e4867a0f732623f8c6a74a2f579de794ae7190b8
ayyappandharshini1221-blip <plicentring9876@gmail.com>
2025-12-09 12:04:30 +0530

    21

--- Java/heapsort.java
+++ Java/heapsort.java
@@ -1,4 +1,5 @@
 public class HeapSort {
+
     static void heapify(int arr[], int n, int i) {
         int largest = i;
         int l = 2 * i + 1;
@@ -22,6 +23,7 @@ public class HeapSort {
 
         for (int i = n / 2 - 1; i >= 0; i--)
             heapify(arr, n, i);
+
         for (int i = n - 1; i > 0; i--) {
             int temp = arr[0];
             arr[0] = arr[i];
@@ -33,6 +35,7 @@ public class HeapSort {
     public static void main(String[] args) {
         int arr[] = { 12, 11, 13, 5, 6, 7 };
         heapSort(arr);
+
         for (int i : arr)
             System.out.print(i + " ");
     }
